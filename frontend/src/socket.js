6c25568739b7e392dd14c93c251cd4ec913c17d5
avinashsingh108 <TsaPh@69@HMS108>
2025-03-25 15:38:11 +0530

    add backend url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {autoConnect: false});
+const socket = io("https://socket-fgvx.onrender.com", { autoConnect: false });
 
 export default socket;
